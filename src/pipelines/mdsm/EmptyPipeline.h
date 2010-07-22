0714e075222ba486463811e0c339e4fc982a940f
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2010-07-22 17:50:41 +0100

    add EmptyPipeline

---
+++ src/pipelines/mdsm/EmptyPipeline.h
@@ -0,0 +1,45 @@
+#ifndef EMPTY_PIPELINE_H
+#define EMPTY_PIPELINE_H
+
+
+#include "pelican/core/AbstractPipeline.h"
+#include "pelican/data/DataBlob.h"
+#include "pelican/output/PelicanTCPBlobServer.h"
+#include "SubbandTimeSeries.h"
+#include "SigprocStokesWriter.h"
+
+/**
+ * @file EmptyPipeline.h
+ */
+
+namespace pelican {
+namespace lofar {
+
+/**
+ * @class EmptyPipeline
+ *
+ * @brief
+ *
+ * @details
+ *
+ */
+class EmptyPipeline : public AbstractPipeline
+{
+    public:
+        EmptyPipeline();
+        ~EmptyPipeline();
+
+        /// Initialises the pipeline.
+        void init();
+
+        /// Runs the pipeline.
+        void run(QHash<QString, DataBlob*>& remoteData);
+
+    private:
+        unsigned _iteration;
+};
+
+} // namespace lofar
+} // namespace pelican
+
+#endif // EMPTY_PIPELINE_H
