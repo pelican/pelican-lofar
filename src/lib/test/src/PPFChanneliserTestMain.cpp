6c7cbfb0b74fda47d421215ff561e1030bca2573
Benjamin Mort <benjamin.mort@oerc.ox.ac.uk>
2010-08-31 16:16:50 +0100

    added a non-cppunit test

---
+++ src/lib/test/src/PPFChanneliserTestMain.cpp
@@ -0,0 +1,129 @@
+#include "test/PPFChanneliserTest.h"
+
+#include "PPFChanneliser.h"
+#include "SpectrumDataSet.h"
+#include "TimeSeriesDataSet.h"
+
+#include "pelican/utility/ConfigNode.h"
+
+#include <QtCore/QTime>
+#include <QtCore/QFile>
+#include <QtCore/QTextStream>
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <cmath>
+
+using std::cout;
+using std::endl;
+namespace P = pelican;
+namespace PL = pelican::lofar;
+
+// Forward declaration of function to create a configuration XML node for
+// the channeliser.
+QString createXMLConfig(unsigned nChannels, unsigned nThreads, unsigned nTaps,
+        const QString& windowType = "kaiser");
+
+
+int main(int /*argc*/, char** /*argv*/)
+{
+    bool verbose = true;
+    unsigned nChannels = 16;
+    unsigned nSubbands = 62;
+    unsigned nPols = 2;
+    unsigned nTaps = 8;
+
+    unsigned timesPerChunk =  512 * 1000;
+
+    if (timesPerChunk%nChannels) CPPUNIT_FAIL("Setup error");
+
+    unsigned _nBlocks = timesPerChunk / nChannels;
+
+    cout << endl << "***** testing PPFChanneliser run() method ***** " << endl;
+    cout << endl;
+    cout << "-------------------------------------------------" << endl;
+    cout << "[PPFChanneliser]: run() " << endl;
+    cout << "- nChan = " << nChannels << endl;
+    if (verbose) {
+        cout << "- nTaps = " << nTaps << endl;
+        cout << "- nBlocks = " << _nBlocks << endl;
+        cout << "- nSubbands = " << nSubbands << endl;
+        cout << "- nPols = " << nPols << endl;
+    }
+
+    // Setup the channeliser.
+    unsigned nThreads = 2;
+    P::ConfigNode config(createXMLConfig(nChannels, nThreads, nTaps));
+
+    PL::PPFChanneliser channeliser(config);
+    PL::SpectrumDataSetC32 spectra;
+
+    // Run once to size up buffers etc.
+    {
+        PL::TimeSeriesDataSetC32 timeSeries;
+        timeSeries.resize(_nBlocks, nSubbands, nPols, nChannels);
+
+        QTime timer;
+        timer.start();
+        channeliser.run(&timeSeries, &spectra);
+        int elapsed = timer.elapsed();
+        cout << "* Time for 1st run = " << elapsed << " ms. [" << nThreads << " threads]" << endl;
+    }
+
+    {
+        PL::TimeSeriesDataSetC32 timeSeries;
+        timeSeries.resize(_nBlocks, nSubbands, nPols, nChannels);
+
+        QTime timer;
+        timer.start();
+        channeliser.run(&timeSeries, &spectra);
+        int elapsed = timer.elapsed();
+
+        cout << "* Time for 2nd run = " << elapsed << " ms. [" << nThreads << " threads]" << endl;
+    }
+
+    {
+        PL::TimeSeriesDataSetC32 timeSeries;
+        timeSeries.resize(_nBlocks, nSubbands, nPols, nChannels);
+
+        QTime timer;
+        timer.start();
+        channeliser.run(&timeSeries, &spectra);
+        int elapsed = timer.elapsed();
+
+        cout << "* Time for 3rd run = " << elapsed << " ms. [" << nThreads << " threads]" << endl;
+    }
+
+
+    {
+        PL::TimeSeriesDataSetC32 timeSeries;
+        timeSeries.resize(_nBlocks, nSubbands, nPols, nChannels);
+
+        QTime timer;
+        timer.start();
+        channeliser.run(&timeSeries, &spectra);
+        int elapsed = timer.elapsed();
+
+        cout << "* Time for 4th run = " << elapsed << " ms. [" << nThreads << " threads]" << endl;
+    }
+
+
+    cout << endl;
+    cout << " (data time = " << _nBlocks * nChannels * 5e-3 << " ms.)" << endl;
+    cout << "-------------------------------------------------" << endl;
+
+    return 0;
+}
+
+
+QString createXMLConfig(unsigned nChannels, unsigned nThreads, unsigned nTaps,
+        const QString& windowType)
+{
+    QString xml =
+            "<PPFChanneliser>"
+            "   <outputChannelsPerSubband value=\"" + QString::number(nChannels) + "\"/>"
+            "   <processingThreads value=\"" + QString::number(nThreads) + "\"/>"
+            "   <filter nTaps=\"" + QString::number(nTaps) + "\" filterWindow=\"" + windowType + "\"/>"
+            "</PPFChanneliser>";
+    return xml;
+}
