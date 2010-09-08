8a241b1fbf3948e185f325f65bc33c85593f7f6a
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2010-09-08 15:33:46 +0100

    Added some basic timer functions.

--- src/lib/timer.h
+++ src/lib/timer.h
@@ -1,10 +1,41 @@
 #ifndef PELICAN_LOFAR_TIMER_H_
 #define PELICAN_LOFAR_TIMER_H_
 
-#include <cstdio>
-#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <sys/time.h>
+
+typedef struct
+{
+	int counter;
+	double timeStart;
+	double timeElapsed;
+	double timeMin;
+	double timeMax;
+	double timeAverage;
+} TimerData;
+
+static inline void timerInit(TimerData* data)
+{
+	data->counter = 0;
+	data->timeStart = 0.0;
+	data->timeElapsed = 0.0;
+	data->timeMin = 0.0;
+	data->timeMax = 0.0;
+	data->timeAverage = 0.0;
+}
+
+static inline void timerReport(TimerData* data, const char* message)
+{
+	printf("--------------------Timer Report--------------------\n");
+	printf("-- %s\n", message);
+	printf("-- Minimum: %.4f sec\n", data->timeMin);
+	printf("-- Maximum: %.4f sec\n", data->timeMax);
+	printf("-- Average: %.4f sec\n", data->timeAverage);
+	printf("-- Counter: %d\n", data->counter);
+	printf("----------------------------------------------------\n");
+}
 
 static inline double timerSec()
 {
@@ -13,5 +44,20 @@ static inline double timerSec()
     return t.tv_sec + (t.tv_usec * 1.0e-6);
 }
 
+static inline void timerStart(TimerData* data)
+{
+	data->timeStart = timerSec();
+	data->timeElapsed = 0.0;
+}
+
+static inline void timerUpdate(TimerData* data)
+{
+	double elapsed = timerSec() - data->timeStart;
+	data->timeElapsed = elapsed;
+	if (elapsed < data->timeMin) data->timeMin = elapsed;
+	if (elapsed > data->timeMax) data->timeMax = elapsed;
+	data->timeAverage = (elapsed + counter * data->timeAverage) / (counter + 1);
+	data->counter++;
+}
 
 #endif
