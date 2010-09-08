0376503364fb3af4d6a41dcf3489fd5b3247a2c1
Benjamin Mort <benjamin.mort@oerc.ox.ac.uk>
2010-09-08 12:27:27 +0100

    updated timer

--- src/lib/timer.h
+++ src/lib/timer.h
@@ -1,11 +1,17 @@
+#ifndef PELICAN_LOFAR_TIMER_H_
+#define PELICAN_LOFAR_TIMER_H_
+
 #include <cstdio>
 #include <cstdlib>
 
 #include <sys/time.h>
 
-double timerSec()
+static inline double timerSec()
 {
     struct timeval t;
     gettimeofday(&t, NULL);
     return t.tv_sec + (t.tv_usec * 1.0e-6);
 }
+
+
+#endif
