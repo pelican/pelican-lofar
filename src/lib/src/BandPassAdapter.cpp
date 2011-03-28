b0b03d973d9552056eaa19b94c51489a3d7e8d95
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2011-03-28 19:24:39 +0100

    bugfix: BandPassAdapter now reads in file correctly

--- src/lib/src/BandPassAdapter.cpp
+++ src/lib/src/BandPassAdapter.cpp
@@ -4,6 +4,7 @@
 #include <QVector>
 #include "BinMap.h"
 #include "BandPass.h"
+#include <iostream>
 
 
 namespace pelican {
@@ -27,24 +28,47 @@ BandPassAdapter::~BandPassAdapter()
 
 void BandPassAdapter::deserialise(QIODevice* device) 
 {
+    int max = 800; // max number of chars per line
     BandPass* blob = (BandPass*) dataBlob();
     // format of data file
+    // # comment
     // uint number of channels
     // float start channel frequency
     // float end channel frequency
     // float channel width
     // list of floats corresponding to the paramterised equation (0...nth coefficient)
-    QByteArray b = device->readLine();
-    unsigned int nChan = b.toUInt();
-    float startFreq = (device->readLine()).toFloat();
-    float endFreq = (device->readLine()).toFloat();
-    float deltaF = (device->readLine()).toFloat();
-    float rms = (device->readLine()).toFloat();
-    float median = (device->readLine()).toFloat();
     QVector<float> params;
-    while( device->canReadLine() ) {
-        params.append( (device->readLine()).toFloat() );
+    char c;
+    
+    int count =0 ,line = 0;
+    while( ! device->atEnd() ) {
+        ++line;
+        device->getChar( &c );
+        if( c == '#' ) {
+            // dump any comments
+            device->readLine(max);
+            continue;
+        }
+        ++count;
+        device->ungetChar(c);
+        QByteArray b = device->readLine(max).trimmed();
+        bool ok;
+        params.append( b.toDouble(&ok) );
+        if( ! ok ) 
+            throw QString("BandPassAdapter: syntax error on line %1").arg(line);
     }
+
+    if( count < 6 ) 
+        throw QString("BandPassAdapter: data format wrong : got %1 parameters, expecting at least 6").arg(count);
+
+    unsigned int nChan = (unsigned int)(params[0]);
+    float startFreq = params[1];
+    float endFreq = params[2];
+    float deltaF = params[3];
+    float rms = params[4];
+    float median = params[5];
+    params.remove(0,6);
+ 
     BinMap map( nChan );
     map.setStart(startFreq);
     map.setBinWidth(deltaF);
