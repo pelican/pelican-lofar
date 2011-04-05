c028e4e89e4acb3650105cdc589123aa4da255ca
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2011-04-05 17:05:09 +0100

    read in bandpass kill filters

--- src/lib/src/BandPassAdapter.cpp
+++ src/lib/src/BandPassAdapter.cpp
@@ -38,21 +38,42 @@ void BandPassAdapter::deserialise(QIODevice* device)
     // float channel width
     // list of floats corresponding to the paramterised equation (0...nth coefficient)
     QVector<float> params;
+    QMap<unsigned int,float> killChannels;
     char c;
     
     int count =0 ,line = 0;
     while( ! device->atEnd() ) {
+        bool ok;
         ++line;
         device->getChar( &c );
-        if( c == '#' ) {
-            // dump any comments
-            device->readLine(max);
-            continue;
+        switch ( c )
+        {
+            case '#':
+                {
+                    // dump any comments
+                    device->readLine(max);
+                    continue;
+                }
+            case 'K':
+                {
+                    QByteArray b = device->readLine(max).simplified();
+                    QList<QByteArray> nums = b.split(' ');
+                    if( nums.size() < 2 ) 
+                        throw QString("BandPassAdapter: syntax error on line %1: expecting two numbers on Kill line: index frequency").arg(line);
+                    unsigned int index = nums[0].toUInt(&ok);
+                    if( ! ok ) 
+                        throw QString("BandPassAdapter: syntax error on line %1 - cannot convert index to unsigned integer").arg(line);
+                    double value = nums[0].toDouble(&ok);
+                    if( ! ok ) 
+                        throw QString("BandPassAdapter: syntax error on line %1 - cannot convert frequency to a double").arg(line);
+                    killChannels.insert(index,value);
+                    continue;
+                }
+            default:
+                device->ungetChar(c);
         }
         ++count;
-        device->ungetChar(c);
         QByteArray b = device->readLine(max).trimmed();
-        bool ok;
         params.append( b.toDouble(&ok) );
         if( ! ok ) 
             throw QString("BandPassAdapter: syntax error on line %1").arg(line);
@@ -63,19 +84,24 @@ void BandPassAdapter::deserialise(QIODevice* device)
 
     unsigned int nChan = (unsigned int)(params[0]);
     float startFreq = params[1];
-    float endFreq = params[2];
+    //float endFreq = params[2];
     float deltaF = params[3];
     float rms = params[4];
     float median = params[5];
     params.remove(0,6);
  
     BinMap map( nChan );
-    map.setStart(startFreq);
+    map.setStart( startFreq - deltaF/2.0 );
     map.setBinWidth(deltaF);
     blob->setData( map, params );
     blob->setMedian(median);
     blob->setRMS(rms);
-
+    foreach( unsigned int index, killChannels ) {
+        // sanity check, inedx matches frequency specified
+        if( map.binIndex(killChannels[index]) != (int)index )
+            throw QString("BandPassAdapter:  Frequency kill mismatch: index %1 does not match frequency %2").arg(index).arg(killChannels[index]);
+        blob->killChannel(index);
+    }
 }
 
 } // namespace lofar
