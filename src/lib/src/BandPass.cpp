7b41fdfd389c3cbc29491b1d32152a1f50ba3158
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2011-05-26 13:37:26 +0100

    bugfix: otectors on empty data sets and out of range

--- src/lib/src/BandPass.cpp
+++ src/lib/src/BandPass.cpp
@@ -63,7 +63,7 @@ void BandPass::_zeroChannelsMap(const BinMap& map)
          int max = map.binIndex(r.max());
          if( max < min ) { int tmp; tmp = max; max = min; min = tmp; };
          int mapId=map.hash();
-         if(_dataSets[mapId].size() < max ) _dataSets[mapId].resize(max);
+         if(_dataSets[mapId].size() < max ) _dataSets[mapId].resize(max + 1);
          do {
              _dataSets[mapId][min] = 0.0;
          } while( ++min <= max );
