3618680154c870c3bfa0dbd66b9287b3da06d865
Chris Williams <chris@cjwilliams.info>
2011-06-17 08:08:50 +0100

    clean up code

--- src/pipelines/udpstream/src/udpBFmainStream2.cpp
+++ src/pipelines/udpstream/src/udpBFmainStream2.cpp
@@ -1,18 +1,7 @@
 #include "UdpBFApplication.h"
-
-//#include "pelican/core/PipelineApplication.h"
-//#include "LofarStreamDataClient.h"
-//#include "BandPassPipeline.h"
-//#include "AdapterTimeSeriesDataSet.h"
-//#include "pelican/core/PipelineSwitcher.h"
-//#include "PumaOutput.h"
-//#include <QtCore/QCoreApplication>
-
+#include <QString>
 #include <iostream>
-#include <map>
 
-using std::cout;
-using std::endl;
 using namespace pelican;
 using namespace pelican::lofar;
 
@@ -24,27 +13,9 @@ int main(int argc, char* argv[])
 
     try {
         UdpBFApplication(argc, argv, stream);
-        /*
-        // Create a PipelineApplication.
-        PipelineApplication pApp(argc, argv);
-
-        // Register the pipelines that can run.
-        //pApp.registerPipeline(new UdpBFPipelineStream2);
-        PipelineSwitcher sw;
-        //sw.addPipeline(new BandPassPipeline(stream));
-        sw.addPipeline(new UdpBFPipeline(stream));
-        pApp.addPipelineSwitcher(sw);
-        //pApp.registerPipeline( new UdpBFPipeline("LofarDataStream2") );
-
-        // Set the data client.
-        pApp.setDataClient("PelicanServerClient");
-
-        // Start the pipeline driver.
-        pApp.start();
-        */
     }
     catch (const QString& err) {
-        cout << "Error caught in updBFmainStream2.cpp: " << err.toStdString() << endl;
+        std::cerr << "Error caught in updBFmainStream2.cpp: " << err.toStdString() << std::endl;
     }
 
     return 0;
