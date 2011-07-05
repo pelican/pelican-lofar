48bee71630634345c3e60f0e4b48db8d69b26f6c
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2011-07-05 18:15:32 +0100

    basic main for SigProc file

---
+++ src/pipelines/udpstream/src/sigprocPipelineMain.cpp
@@ -0,0 +1,35 @@
+#include "pelican/core/PipelineApplication.h"
+#include "pelican/core/FileDataClient.h"
+#include "SigProcPipeline.h"
+
+#include <QtCore/QCoreApplication>
+
+#include <iostream>
+
+using namespace pelican;
+using namespace pelican::lofar;
+
+int main(int argc, char* argv[])
+{
+    // Create a QCoreApplication.
+    QCoreApplication app(argc, argv);
+
+    try {
+        // Create a PipelineApplication.
+        PipelineApplication pApp(argc, argv);
+
+        // Register the pipelines that can run.
+        pApp.registerPipeline(new SigProcPipeline);
+
+        // Set the data client.
+        pApp.setDataClient("FileDataClient");
+
+        // Start the pipeline driver.
+        pApp.start();
+    }
+    catch (const QString& error) {
+        std::cout << "Error: " << error.toStdString() << std::endl;
+    }
+
+    return 0;
+}
