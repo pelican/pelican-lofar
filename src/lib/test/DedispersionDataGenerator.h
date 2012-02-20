34fdb21a4be0f46aa1736c23e4a4bdc6aa3775f6
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2012-02-20 11:23:23 +0000

    add Dedispersion dummy data generator class to test utility library

---
+++ src/lib/test/DedispersionDataGenerator.h
@@ -0,0 +1,58 @@
+#ifndef DEDISPERSIONDATAGENERATOR_H
+#define DEDISPERSIONDATAGENERATOR_H
+
+#include <QList>
+
+/**
+ * @file DedispersionDataGenerator.h
+ */
+
+namespace pelican {
+
+namespace lofar {
+class SpectrumDataSetStokes;
+
+/**
+ * @class DedispersionDataGenerator
+ *  
+ * @brief
+ *    Generate test dedispersed signal data
+ * @details
+ * 
+ */
+
+class DedispersionDataGenerator
+{
+    public:
+        DedispersionDataGenerator(  );
+        ~DedispersionDataGenerator();
+
+        /// generate a dataset with a single dedispersion signal embedded
+        //  The caller is the owner of the resulting data (seed deleteData)
+        QList<SpectrumDataSetStokes*> generate( int numberOfBlocks , float dedispersionMeasure );
+
+        /// return the duration (in seconds) of each time sample
+        double timeOfSample() const { return tsamp; };
+
+        /// return the value of the starting Frequency
+        double startFrequency() const { return fch1; };
+
+        /// write a data set to the standard pelican Datablob output file format
+        void writeToFile( const QString& filename, const QList<SpectrumDataSetStokes*>& data );
+
+        /// convenience method to clean up the memory of a generated dataset
+        static void deleteData( QList<SpectrumDataSetStokes*>& data );
+
+    protected:
+        unsigned nSamples; // number of samples per DataBlob
+        unsigned nSubbands;
+        unsigned nChannels; 
+
+        double fch1; // frequency of channel 1
+        double foff; // frequency delta (assumed to be -ve)
+        double tsamp; // time sample length (seconds)
+};
+
+} // namespace lofar
+} // namespace pelican
+#endif // DEDISPERSIONDATAGENERATOR_H 
---
+++ src/lib/test/src/DedispersionDataGenerator.cpp
@@ -0,0 +1,84 @@
+#include "DedispersionDataGenerator.h"
+#include "pelican/utility/ConfigNode.h"
+#include "pelican/output/DataBlobFile.h"
+#include "SpectrumDataSet.h"
+
+
+namespace pelican {
+
+namespace lofar {
+
+
+/**
+ *@details DedispersionDataGenerator 
+ */
+DedispersionDataGenerator::DedispersionDataGenerator()
+{
+    // set default values (typical LOFAR station values)
+    nSamples = 16; // samples per blob
+    nSubbands = 32;
+    nChannels = 64; // 2048 total channels (32x64)
+
+    fch1 = 150;
+    foff = -6.0/(double)(nSubbands*nChannels);
+    tsamp = 0.00032768; // time sample length (seconds)
+}
+
+/**
+ *@details
+ */
+DedispersionDataGenerator::~DedispersionDataGenerator()
+{
+}
+
+QList<SpectrumDataSetStokes*> DedispersionDataGenerator::generate( int numberOfBlocks , float dm )
+{
+    QList<SpectrumDataSetStokes*> data;
+
+    for( int i=0; i < numberOfBlocks; ++i ) {
+        SpectrumDataSetStokes* stokes = new SpectrumDataSetStokes;
+        stokes->resize(nSamples, nSubbands, 1, nChannels);
+        data.append(stokes);
+
+        int offset = i * nSamples;
+        //stokes->setLofarTimestamp(channeliserOutput->getLofarTimestamp());
+        for (unsigned int t = 0; t < nSamples; ++t ) {
+            for (unsigned s = 0; s < nSubbands; ++s ) {
+                for (unsigned c = 0; c < nChannels; ++c) {
+                    int absChannel = s * nChannels + c;
+                    int index = (int)( dm * (4148.741601 * ((1.0 / (fch1 + (foff * absChannel)) /
+                        (fch1 + (foff * absChannel))) - (1.0 / fch1 / fch1))/tsamp ) );
+                    int sampleNumber = index - offset;
+
+                    float* I = stokes->spectrumData(t, s, 0);
+                    if( sampleNumber == (int)t ) {
+                        I[c] = 1.0;
+                    } else {
+                        I[c] = 0.0;
+                    }
+                }
+            }
+        }
+    }
+    return data;
+}
+
+void DedispersionDataGenerator::writeToFile( const QString& filename, const QList<SpectrumDataSetStokes*>& data ) {
+    ConfigNode dummy;
+    DataBlobFile writer(dummy);
+    writer.addFile(filename ,DataBlobFileType::Homogeneous);
+    foreach( const SpectrumDataSetStokes* d, data ) {
+        writer.send( QString("input"), d );
+    }
+
+}
+
+void DedispersionDataGenerator::deleteData( QList<SpectrumDataSetStokes*>& data )
+{
+    foreach( SpectrumDataSetStokes* d, data ) {
+        delete d;
+    }
+}
+
+} // namespace lofar
+} // namespace pelican
