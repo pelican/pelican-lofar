5975f3fad31637f6d7e4f7987c6dfcb824eda363
Jayanth Chennamangalam <jayanthc@gmail.com>
2014-09-23 15:00:52 +0000

    Added working test emulator.

---
+++ src/lib/test/src/JTestEmulatorMain.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <QtCore/QCoreApplication>
+#include "pelican/emulator/EmulatorDriver.h"
+#include "JTestEmulator.h"
+
+using namespace pelican;
+using namespace pelican::ampp;
+
+int main(int argc, char* argv[])
+{
+    // Create a QCoreApplication
+    QCoreApplication app(argc, argv);
+
+    try {
+        ConfigNode emulatorConfig("<JTestEmulator>"
+                                  "<packet samples=\"256\" interval=\"2560\" />"
+                                  "<signal period=\"20\" />"
+                                  "<connection host=\"127.0.0.1\" port=\"2001\" />"
+                                  "</JTestEmulator>");
+        EmulatorDriver emulator(new JTestEmulator(emulatorConfig));
+    }
+
+    // Catch any error messages from Pelican
+    catch (const QString& err) {
+        std::cerr << "Error: " << err.toStdString() << std::endl;
+    }
+
+    return 0;
+}
+
