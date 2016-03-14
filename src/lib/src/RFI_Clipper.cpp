a21c255c9cf8942beef7290298eab2133dc0ff4f
artemis <artemis@vivace1.localdomain>
2016-03-14 14:11:43 +0000

    further fixes to RFI_Clipper

--- src/lib/src/RFI_Clipper.cpp
+++ src/lib/src/RFI_Clipper.cpp
@@ -21,7 +21,7 @@ namespace ampp {
  */
 RFI_Clipper::RFI_Clipper( const ConfigNode& config )
   : AbstractModule( config ), _active(true), _crFactor(10.0),_srFactor(4.0), _current(0),
-    _badSpectra(0)
+    _badSpectra(0), _fractionBadChannels(0)
 {
     _current = 0;
     if( config.hasAttribute("active") &&
@@ -67,6 +67,9 @@ RFI_Clipper::RFI_Clipper( const ConfigNode& config )
     _zeroDMing = 0;
     if( config.getOption("zeroDMing", "active" ) == "true" ) {
       _zeroDMing = 1;
+    }
+    if( config.getOption("computeRMSfromMean", "active", "true" ) == "true" ) {
+      _useMeanOverRMS = 1;
     }
     _startFrequency = 0.0;
     _endFrequency = 0.0;
@@ -137,8 +140,9 @@ static inline void clipSample( SpectrumDataSetStokes* stokesAll, float* W, unsig
                 t, nChannels );
         for (unsigned c = 0; c < nChannels; ++c) {
 	  //            I[index + c] = 0.0;
-            W[index + c] = 1.0;
-            I[index + c] = lastGoodSpectrum[s*nChannels + c];
+	  W[index + c] = 1.0;
+          //  W[index + c] = 0.0;
+	  I[index + c] = lastGoodSpectrum[s*nChannels + c];
         }
       }
     }
@@ -162,6 +166,10 @@ void RFI_Clipper::run( WeightedSpectrumDataSet* weightedStokes )
     unsigned nPolarisations = stokesAll->nPolarisations();
     unsigned nBins = nChannels * nSubbands;
     unsigned goodSamples = 0;
+    float k = 4; // degrees of freedom
+    float meanMinusMinimum = k / sqrt(2.0*k); 
+    float spectrumRMS;
+    float dataModel;
 
     if (_lastGoodSpectrum.size() != nBins) _lastGoodSpectrum.resize(nBins);
 
@@ -177,14 +185,14 @@ void RFI_Clipper::run( WeightedSpectrumDataSet* weightedStokes )
       //      std::cout << "Next spectrum " << _num << std::endl;
 
       const QVector<float>& bandPass = _bandPass.currentSet();
-      // The following is the amount of tolerance to changes in the average value of the spectrum
       float spectrumSum = 0.0;
       float spectrumSumSq = 0.0;
-      //      float goodChannels = 0.0;
+      // Split the spectrum into 8 bands for the purpose of matching
+      // the spectrum to the model
       std::vector<float> goodChannels(8,0.0);
-      // Split the spectrum into 8 bands for the purpose of matching the spectrum to the model
       unsigned channelsPerBand = nBins / 8;
-      // find the minima of I in each band, and the minima of the model and compare
+      // find the minima of I in each band, and the minima of the
+      // model and compare
       std::vector<float> miniData(8,1e6);
       std::vector<float> miniModel(8,1e6);
       std::vector<float> dataMinusModel(8);
@@ -194,72 +202,103 @@ void RFI_Clipper::run( WeightedSpectrumDataSet* weightedStokes )
 
       // Find the data minima and model minima in each band
       // Let us also estimate sigma in each band
-      for (unsigned s = 0; s < nSubbands; ++s) {
-        long index = stokesAll->index(s, nSubbands,
-				      0, nPolarisations,
-				      t, nChannels );
-        for (unsigned c = 0; c < nChannels ; ++c) {
-          int binLocal = s*nChannels +c; 
-	  unsigned band = (int)binLocal / channelsPerBand;
-
-	  //	  std::cout << "----------------" << std::endl;
-	  //	  std:: cout << band << " " << binLocal << " " << I[index+c] << std::endl;
-
-	  if (I[index+c] < miniData[band]) miniData[band] = I[index+c];
-	  if (bandPass[binLocal] < miniModel[band]) miniModel[band] = bandPass[binLocal];
-	  bandMean[band] += I[index+c];
-	  bandMeanSquare[band] += (I[index+c]*I[index+c]);
-        }
-      }
       
-      // Now find the distances between data and model and the RMSs in
-      // each band
+      // Try this over an adapting stage, lasting as long as the running average buffers
+      if (!_rmsBuffer.full()){
+	for (unsigned s = 0; s < nSubbands; ++s) {
+	  long index = stokesAll->index(s, nSubbands,
+					0, nPolarisations,
+					t, nChannels );
+	  for (unsigned c = 0; c < nChannels ; ++c) {
+	    int binLocal = s*nChannels +c; 
+	    unsigned band = (int)binLocal / channelsPerBand;
+	    
+	    //	  std::cout << "----------------" << std::endl; std::
+	    //	  cout << band << " " << binLocal << " " << I[index+c]
+	    //	  << std::endl;
+	    
+	    if (I[index+c] < miniData[band]) miniData[band] = I[index+c];
+	    if (bandPass[binLocal] < miniModel[band]) miniModel[band] = bandPass[binLocal];
+	    bandMean[band] += I[index+c];
+	    bandMeanSquare[band] += (I[index+c]*I[index+c]);
+	  }
+	}
+	
+	// Now find the distances between data and model and the RMSs in
+	// each band
+	
+	for (unsigned b = 0; b < 8; ++b){
+	  dataMinusModel[b] = miniData[b] - miniModel[b];
+	  bandSigma[b] = sqrt(bandMeanSquare[b]/channelsPerBand - std::pow(bandMean[b]/channelsPerBand,2));
+	  //std::cout << bandSigma[b] << " " << bandMean[b] << " " <<
+	  //bandMeanSquare[b] << std::endl;
+	}
+	// Assume the minimum bandSigma to be the best estimate of this
+	// spectrum RMS
+	spectrumRMS = *std::min_element(bandSigma.begin(), bandSigma.end());
 
-      for (unsigned b = 0; b < 8; ++b){
-	dataMinusModel[b] = miniData[b] - miniModel[b];
-	bandSigma[b] = sqrt(bandMeanSquare[b]/channelsPerBand - std::pow(bandMean[b]/channelsPerBand,2));
-	//std::cout << bandSigma[b] << " " << bandMean[b] << " " << bandMeanSquare[b] << std::endl;
-      }
-      // Assume the minimum bandSigma to be the best estimate of this spectrum RMS
-      float spectrumRMS = *std::min_element(bandSigma.begin(), bandSigma.end());
+	// Take the median of dataMinusModel to determine the distance
+	// from the model
+	std::nth_element(dataMinusModel.begin(), dataMinusModel.begin()+dataMinusModel.size()/2, dataMinusModel.end());
+	dataModel = (float)*(dataMinusModel.begin()+dataMinusModel.size()/2);
+	//      std::cout << "data minus model " << dataModel << " " <<
+	// std::endl; since we have used the minima to determine this
+	// distance, we assume that dataModel is actually k/sqrt(k)
+	// sigma away from the real value, where k is the number of the
+	// degrees of freedom of the chi-squared distribution of the
+	// incoming data. For no integration, k will be 4 (2 powers per
+	// poln)
+	
+	//      std::cout << "dataModel " << dataModel << std::endl;
+	
+	// Let us now build up a running average of spectrumRMS values
+	// (_maxHistory of them)
+	
+	// if the buffer is not full, compute the new rmsRunAve like this
+	// if (!_rmsBuffer.full()) {
+	
+	_rmsBuffer.push_back(spectrumRMS);
+	_rmsRunAve = std::accumulate(_rmsBuffer.begin(), _rmsBuffer.end(), 0.0)/_rmsBuffer.size();
+	
+	// The very last time this is done, store a reference value of
+	// the mean over the rms; this works as I have just added on the
+	// last value two lines above
+	if (_rmsBuffer.full()) _meanOverRMS = _meanRunAve / _rmsRunAve;
+	
+	// and update the model
+	dataModel = dataModel + meanMinusMinimum * _rmsRunAve;
 
-      // Take the median of dataMinusModel to determine the distance from the model
-      std::nth_element(dataMinusModel.begin(), dataMinusModel.begin()+dataMinusModel.size()/2, dataMinusModel.end());
-      float dataModel = (float)*(dataMinusModel.begin()+dataMinusModel.size()/2);
-      //      std::cout << "data minus model " << dataModel << " " <<
-      // std::endl; since we have used the minima to determine this
-      // distance, we assume that dataModel is actually k/sqrt(k)
-      // sigma away from the real value, where k is the number of the
-      // degrees of freedom of the chi-squared distribution of the
-      // incoming data. For no integration, k will be 4 (2 powers per
-      // poln)
-
-      //      std::cout << "dataModel " << dataModel << std::endl;
-
-      // Let us now build up a running average of spectrumRMS values
-      // (_maxHistory of them)
-
-      // if the buffer is not full, compute the new rmsRunAve like this
-      if (!_rmsBuffer.full()) {
-
-      _rmsBuffer.push_back(spectrumRMS);
-      _rmsRunAve = std::accumulate(_rmsBuffer.begin(), _rmsBuffer.end(), 0.0)/_rmsBuffer.size();
-      
+			
       }
       else {
-	//just update the running average with the new value, and take the oldest off the end
-	//	std::cout << "History buffer now full " << _num << std::endl;
+	// just update the running average with the current last value,
+	// and take the oldest off the end. Then add the new
+	// value onto the buffer. The idea here is that the new
+	// value is not used for the current spectrum, but rather
+	// for the one after. The current spectrum is evaluated
+	// based on the rms values in the buffer up to that
+	// point.
+
 	_rmsRunAve -= (_rmsBuffer.front()/_rmsBuffer.size());
-	_rmsBuffer.push_back(spectrumRMS);
 	_rmsRunAve += (_rmsBuffer.back()/_rmsBuffer.capacity());
+
+	// In extreme RFI cases, the measured RMS may start growing
+	// due to particular RFI signals. The mean over rms ratio
+	// should remain approximately constant over the course of the
+	// observation. Use this as a safety check, after the mean has
+	// been reasonably determined, to set the RMS to a more
+	// reliable value :
+	if  (_useMeanOverRMS)
+	  //recover the rms running average
+	  spectrumRMS = _meanRunAve / _meanOverRMS;
+
+	_rmsBuffer.push_back(spectrumRMS);
+	// use the mean running average as a model of the mean of the
+	// data; remember that the running average is updated with a
+	// mean after channel clipping, below.
+	dataModel = _meanRunAve;
       }
       
-
-      // and update the model
-      float k = 4;
-      float meanMinusMinimum = k / sqrt(2.0*k); 
-	
-      dataModel = dataModel + meanMinusMinimum * _rmsRunAve;
       // now use this rms to define a margin of tolerance for bright
       // channels
       float margin = _crFactor * _rmsRunAve;
@@ -282,6 +321,7 @@ void RFI_Clipper::run( WeightedSpectrumDataSet* weightedStokes )
 					    pol, nPolarisations, t, nChannels );
 	      I[index + c] = _lastGoodSpectrum[binLocal];
 	      W[index +c] = 1.0;
+	      //	      W[index +c] = 0.0;
 	    }
 	  }
 	  else{
@@ -294,27 +334,41 @@ void RFI_Clipper::run( WeightedSpectrumDataSet* weightedStokes )
       // So now we have the mean of the incoming data, in a reliable
       // form after channel clipping
       unsigned totalGoodChannels=std::accumulate(goodChannels.begin(), goodChannels.end(), 0);
+      _fractionBadChannels += (float)(nBins - totalGoodChannels)/nBins;
       spectrumSum /= totalGoodChannels;
 
-      // Check if more than 20% of of the channels in each band were
-      // bad. If so in more than half of the bands, set a flag to
-      // true.
+      // Check if more than 20% of the channels in each band were
+      // bad. If so in more than half of the bands, 4 in this case,
+      // keep record. Also, if one band is completely gone, or less
+      // than 80% of the total survive, keep record.
+      
+
       unsigned badBands = 0; 
       for (unsigned b = 0; b < 8; ++b){
 	if (goodChannels[b] < 0.8 * channelsPerBand) {
-	  if (++badBands == 4 ) break;
+	  ++badBands;
+	}
+	if (goodChannels[b] == 0) {
+	  badBands += 4;
 	}
       }
+      if (totalGoodChannels < 0.8 * nBins) badBands +=4;
 
-      //      std::cout << "Current model, mean and rms: " << _bandPass.mean() << " " << _bandPass.rms() << std::endl;
-      //      std::cout << "Current data, mean and rms: " << spectrumSum << " " << spectrumRMS << std::endl;
-      //      std::cout << "new dataModel: " << dataModel << std::endl;
+      //      std::cout << "Current model, mean and rms: " <<
+      //      _bandPass.mean() << " " << _bandPass.rms() << std::endl;
+      //      std::cout << "Current data, mean and rms: " <<
+      //      spectrumSum << " " << spectrumRMS << std::endl;
+      //      std::cout << "new dataModel: " << dataModel <<
+      //      std::endl;
 
-      //      std::cout << "good channels " << goodChannels << std::endl;
+      //      std::cout << "good channels " << goodChannels <<
+      //      std::endl;
       
+
+
       // Let us now build up the running average of spectrumSum values
-      // (_maxHistory of them)
-      // if the buffer is not full, compute the new meanRunAve like this
+      // (_maxHistory of them) if the buffer is not full, compute the
+      // new meanRunAve like this
 
 
       if (!_meanBuffer.full()) {
@@ -322,11 +376,18 @@ void RFI_Clipper::run( WeightedSpectrumDataSet* weightedStokes )
 	_meanRunAve = std::accumulate(_meanBuffer.begin(), _meanBuffer.end(), 0.0)/_meanBuffer.size();
       }
       else {
-	//   just update the running average with the new value, and take the oldest off the end
+	//   just update the running average with the new value, and
+	//   take the oldest off the end, using the same principle as
+	//   with the rms buffer, i.e. do not use the current
+	//   measurement for the current spectrum.
+
+	// Note there is a tiny descrepance at the point when the
+	// buffer is first full
+
 	//	std::cout << "History buffer now full " << _num << std::endl;
 	_meanRunAve -= _meanBuffer.front()/_meanBuffer.size();
-      	_meanBuffer.push_back(spectrumSum);
       	_meanRunAve += _meanBuffer.back()/_meanBuffer.size();
+      	_meanBuffer.push_back(spectrumSum);
       }
 
       // Now we can check if this spectrum has an abnormally high mean
@@ -339,28 +400,51 @@ void RFI_Clipper::run( WeightedSpectrumDataSet* weightedStokes )
 
       //Now check, if spectrumSum - model > tolerance, declare this
       //time sample useless, replace its data and take care of the
-      //running averages, also cut the first 10 spectra, also cut
-      //spectra where badBands == 4, see above
+      //running averages, also cut the first 1000 spectra, also cut
+      //spectra where badBands >= 4, see above
 
-      if (spectrumSum - _meanRunAve > spectrumRMStolerance || _meanBuffer.size() < 10 || badBands == 4) {
+      if (_meanBuffer.size() < 1000) {
+	// clip the sample, but continue to build the stats; this
+	// helps the stats converge
+	clipSample( stokesAll, W, t, _lastGoodSpectrum );
+      }
+      else if (spectrumSum - _meanRunAve > spectrumRMStolerance || badBands >= 4) {
 
 	// we need to remove this entire spectrum
 	clipSample( stokesAll, W, t, _lastGoodSpectrum );
-	// now remove the last samples from the running averages
-	//	_meanBuffer.pop_back();
-	//	_rmsBuffer.pop_back();
+	// keep a record of bad spectra
+	++_badSpectra;
+
+	// now remove the last samples from the running average
+	// buffers and replace them with the values of the
+	// lastgoodspectrum
+	_meanBuffer.pop_back();	
+	_rmsBuffer.pop_back();
+	_meanBuffer.push_back(_lastGoodMean);
+	_rmsBuffer.push_back(_lastGoodRMS);
       }
-      // else keep a copy of the original spectrum, as it is good!
+      // else keep a copy of the original spectrum, as it is good, but
+      // clip everything down to 1 sigma
       else {
+	spectrumSum = 0.0; // SumSq is still zero
 	for (unsigned s = 0; s < nSubbands; ++s) {
 	  long index = stokesAll->index(s, nSubbands,
 					0, nPolarisations,
 					t, nChannels );
 	  for (unsigned c = 0; c < nChannels; ++c) {
 	    int binLocal = s*nChannels +c;
-	    _lastGoodSpectrum[binLocal] = I[index+c];
+	    if (I[index+c] - dataModel - bandPass[binLocal] 
+		< 3.0 * _rmsRunAve) {
+	      _lastGoodSpectrum[binLocal] = I[index+c];	    
+	    }
+	    spectrumSum += _lastGoodSpectrum[binLocal];
+	    spectrumSumSq += _lastGoodSpectrum[binLocal] * 
+	      _lastGoodSpectrum[binLocal];
 	  }
 	}
+	// and keep the mean and rms values as computed
+	_lastGoodMean = spectrumSum / nBins;
+	_lastGoodRMS = sqrt(spectrumSumSq/nBins - std::pow(_lastGoodMean,2));
       }
       
       
@@ -382,7 +466,7 @@ void RFI_Clipper::run( WeightedSpectrumDataSet* weightedStokes )
 	  //std::cout << "here2" << std::endl;
 	  spectrumSum += I[index+c];
 	  //std::cout << "here3" << std::endl;
-	    spectrumSumSq += I[index+c]*I[index+c];
+	  spectrumSumSq += I[index+c]*I[index+c];
 	}
       }
 
@@ -393,7 +477,8 @@ void RFI_Clipper::run( WeightedSpectrumDataSet* weightedStokes )
       //std::cout << "Values used for normalization" << std::endl;
       //std::cout << spectrumSum << " " << spectrumRMS << std::endl;
 
-      // Avoid nastiness in those first 10 spectra by avoiding divisions by zero
+      // Avoid nastiness in those first spectra by avoiding divisions
+      // by zero
       if (spectrumRMS == 0.0) spectrumRMS = 1.0;
       
       
@@ -403,25 +488,49 @@ void RFI_Clipper::run( WeightedSpectrumDataSet* weightedStokes )
 				      t, nChannels );
 	for (unsigned c = 0; c < nChannels; ++c) {
 	  I[index+c] -= _zeroDMing * spectrumSum;
+	  // it may be better to normalize by the running average RMS,
+	  // given this is a sensitive operation. For example, an
+	  // artificially low rms may scale things up
 	  I[index+c] /= spectrumRMS;
+	  //	  I[index+c] /= _rmsRunAve;
+	  // make sure this division is not introducing signals that
+	  // you would have clipped
+	  if (I[index+c] > _crFactor) I[index+c] = 0.0; 
 	}
       }
       // The bandpass is flat and the spectrum clean and normalized,
-      // so move to the next spectrum
-      // write out some stats:
+      // so move to the next spectrum and write out some stats:
+      unsigned reportStatsEvery = 10 * _maxHistory;
       if (_num == 0) {
-	std::cout << "RFI report ###############################" << std::endl;
-	std::cout << "Running average of mean is at: " << _meanRunAve << std::endl;
-	std::cout << "Running average of rms is at: " << _rmsRunAve << std::endl;
-	std::cout << "###############################" << std::endl;
-	std::cout << "#" << std::endl;
-	std::cout << "#" << std::endl;
+	// calculate fractions
+	float fractionBadSpectra = 100.0 * (float)_badSpectra / (float)reportStatsEvery; 
+	float fractionBadChannels = 100.0 * _fractionBadChannels / (float)reportStatsEvery ;
+
+	// if the fraction of bad spectra becomes >99%, then empty the
+	// circular buffers and go into learning mode again
+	if (fractionBadSpectra > 99.0) {
+	  _rmsBuffer.resize(0);
+	  _meanBuffer.resize(0);
+	  std::cout << "Lost track of the RFI model, retraining.";
+	}
+	
+	std::cout << std::endl;
+	//	std::cout << "# Mean RMS %badSpectra %badChannels : "
+	//	<< std::endl;
+	std::cout <<  "RFIstats: " << _meanRunAve << " " << _rmsRunAve << " " 
+		  << fractionBadSpectra << " " << fractionBadChannels << std::endl;
+	std::cout << std::endl;
+
+	// Reset _bad
+	_badSpectra = 0;
+	_fractionBadChannels = 0.0;
+
       }    
       // and update the model
       _bandPass.setMean(_meanRunAve);
       _bandPass.setRMS(_rmsRunAve);
       ++_num;
-      _num = _num % _maxHistory;
+      _num = _num % reportStatsEvery;
       
     }
     // set the stats of the chunk
